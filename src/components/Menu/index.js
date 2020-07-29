d220e6d9b567b975d30f1ce2fdb6f9975ae57bed
Jussara Soares <jussara.ac.s@hotmail.com>
2020-07-29 01:04:07 -0300

    first settings

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import Button from '../Button';
+// import ButtonLink from '../components/ButtonLink';
+import "./Menu.css";
 
 function Menu() {
   return (
-    <nav>
-      CookBook
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="CookBook Logo" />
+      </a>
+      <Button href="/" as="a" >
+        Novo vídeo
+      </Button>
     </nav>
   )
 }
