c76a0312f840f44b16a09c4fa6e827ab6e51a192
Jussara Soares <jussara.ac.s@hotmail.com>
2020-07-30 17:48:01 -0300

    update category registration

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,26 +4,74 @@ import PageDefault from '../../../components/PageDefault';
 import { useState } from 'react';
 
 function CadastroCategoria() {
-  const [nomeDaCategoria, setNomeDaCategoria] = useState('Value Inicial');
+  const [categorias, setCategorias] = useState(['Teste']);
+  const valoresIniciais = {
+    nome: '',
+    descrição: '',
+    cor: '#000'
+  }
+  const [values, setValues] = useState(valoresIniciais);
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form>
+      <form onSubmit={function HandleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values
+        ]);
+      }}>
+        <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome}
+              onChange={function funcaoHandler(infosDoEvento) {
+                // setNomeDaCategoria(infosDoEvento.target.value)
+              }}
+            />
+          </label>
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-            value={nomeDaCategoria}
-          />
-        </label>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descrição}
+              onChange={function funcaoHandler(infosDoEvento) {
+                // setNomeDaCategoria(infosDoEvento.target.value)
+              }}
+            />
+          </label>
+
+          <label>
+            Color:
+            <input
+              type="color"
+              value={values.cor}
+              onChange={function funcaoHandler(infosDoEvento) {
+                // setNomeDaCategoria(infosDoEvento.target.value)
+              }}
+            />
+          </label>
+        </div>
 
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
 
       <Link to="/">
